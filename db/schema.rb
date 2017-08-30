8c1b7018ca55d99b7512e5fa639420901b3f4332
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-08-30 11:10:55 +0900

    Likeモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829053731) do
+ActiveRecord::Schema.define(version: 20170830015843) do
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "location_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
---
+++ spec/factories/likes.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :like do
+    user_id 1
+    location_id 1
+  end
+end
