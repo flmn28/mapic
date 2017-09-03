710052e00f4a4ff7960a82d5412db358260faa7c
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-09-03 15:17:36 +0900

    likes_controllerのテストをAjax化に対応

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -9,29 +9,29 @@ RSpec.describe LikesController, type: :controller do
   end
 
   describe "POST #like" do
-    xit "create new like" do
-      post :like, params: { location_id: @location.id }, session: valid_session
+    it "create new like" do
+      post :like, params: { location_id: @location.id }, session: valid_session, xhr: true
       expect(Like.all.count).to eq 1
       expect(Like.first.user_id).to eq 1
       expect(Like.first.location_id).to eq 1
     end
 
-    xit "set correct location" do
-      post :like, params: { location_id: @location.id }, session: valid_session
+    it "set correct location" do
+      post :like, params: { location_id: @location.id }, session: valid_session, xhr: true
       expect(assigns(:location)).to eq @location
     end
   end
 
   describe "DELETE #unlike" do
-    xit "destroy one like" do
+    it "destroy one like" do
       create :like, user_id: 1, location_id: 1
-      delete :unlike, params: { location_id: @location.id }, session: valid_session
+      delete :unlike, params: { location_id: @location.id }, session: valid_session, xhr: true
       expect(Like.all.count).to eq 0
     end
 
-    xit "set correct location" do
+    it "set correct location" do
       create :like, user_id: 1, location_id: 1
-      delete :unlike, params: { location_id: @location.id }, session: valid_session
+      delete :unlike, params: { location_id: @location.id }, session: valid_session, xhr: true
       expect(assigns(:location)).to eq @location
     end
   end
