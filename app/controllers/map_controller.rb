aaef71fb30f5fca9f8688afb515136e19f34b7e5
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-10-20 23:27:54 +0900

    MapControllerのoptionアクションをリファクタ

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,45 +5,22 @@ class MapController < ApplicationController
   end
 
   def option
-    params_array = Tag.all.pluck(:id).map { |id| params["tag" + id.to_s] }
-
-    if !params[:myself] && !params[:like] && params_array == Array.new(params_array.count)
-      return @locations = Location.all
-    end
+    params_array = Tag.all.map { |tag| params["tag" + tag.id.to_s] }
+    return @locations = Location.all if !params[:myself] && !params[:like] && params_array == Array.new(params_array.count)
 
     selected_location_ids_array = []
-    if params[:myself]
-      ids = current_user.locations.pluck(:id)
-      selected_location_ids_array.concat(ids)
-    end
-    if params[:like]
-      ids = current_user.like_locations.pluck(:id)
-      selected_location_ids_array.concat(ids)
-    end
+    selected_location_ids_array.concat(current_user.locations.pluck(:id)) if params[:myself]
+    selected_location_ids_array.concat(current_user.like_locations.pluck(:id)) if params[:like]
     selected_location_ids = selected_location_ids_array.uniq
 
     tagged_location_ids_array = []
     params_array.each_with_index do |param, i|
-      if param
-        ids = Tag.find_by(id: i + 1).locations.pluck(:id)
-        tagged_location_ids_array.concat(ids)
-      end
+      tagged_location_ids_array.concat(Tag.find_by(id: i + 1).locations.pluck(:id)) if param
     end
     tagged_location_ids = tagged_location_ids_array.uniq
 
-    if selected_location_ids.length > 0 && tagged_location_ids.length > 0
-      location_ids = selected_location_ids & tagged_location_ids
-    elsif selected_location_ids.length > 0
-      location_ids = selected_location_ids
-    elsif tagged_location_ids.length > 0
-      location_ids = tagged_location_ids
-    end
-
-    @locations = []
-    location_ids.each do |id|
-      location = Location.find_by(id: id)
-      @locations << location
-    end
+    location_ids = selected_location_ids.present? && tagged_location_ids.present? ? selected_location_ids & tagged_location_ids : selected_location_ids + tagged_location_ids
+    @locations = location_ids.map { |id| Location.find_by(id: id) }
   end
 
 end
